03772a4b0b0423e6f03ebd92d788551aba48cf90
naifos60 <naifos60@hotmail.com>
2023-06-07 14:24:40 +0200

    mise en place architecture et dépendance

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo.svg';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Kasa
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
