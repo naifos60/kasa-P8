f77556815c657db92f3b707e64e2b7e27d7bee98
naifos60 <naifos60@hotmail.com>
2023-06-14 17:07:17 +0200

    feature animation fleche collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import dropUp from "../../assets/dropUP.png";
-import dropDown from "../../assets/dropDown.png";
+// import dropDown from "../../assets/dropDown.png";
 
 function Collapse({title, content}){
     const [open, setOpen] = useState(false); 
@@ -13,7 +13,7 @@ function Collapse({title, content}){
         <div className={`collapse ${open && 'active'}`}>
             <div className="collapse_title">
                 <h4>{title}</h4>
-                <button><img src= {`${open ? dropDown : dropUp}`} alt="fleche d'ouverture" className="collapse_icon" onClick={handleToggle} /></button>
+                <button><img src= {dropUp} alt="fleche d'ouverture" className={`collapse_icon ${open && "active"}`} onClick={handleToggle} /></button>
              </div>
             <div className={`collapse_content ${open && "active"}`}>
                 <p className={`collapse_content-text ${open && "active"}`}>{content}</p>
