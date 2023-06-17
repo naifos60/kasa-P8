b11490628b8693c6a72cc42daca04ed449581817
naifos60 <naifos60@hotmail.com>
2023-06-17 18:53:51 +0200

    feature test arrow

--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -1,8 +1,8 @@
-function Title({id, title, location}){
+function Title({title, location}){
     return(
-        <div key={id} className="loc_info">
-                <h1 className="loc_info-title">{title}</h1>
-                <p className="loc_info-location">{location}</p>       
+        <div>
+            <h1 className="loc_info-title">{title}</h1>
+            <p className="loc_info-location">{location}</p>       
         </div>
     )
 }
--- src/pages/Fiches/fiches.jsx
+++ src/pages/Fiches/fiches.jsx
@@ -3,33 +3,45 @@ import Tags from "../../components/Tags/tags.jsx";
 import Collapse from "../../components/Collapse/collapse.jsx";
 import Title from "../../components/Title/title.jsx";
 import data from '../../data/logements.jsx';
+import arrowLeft from '../../assets/arrowLeft.png';
+import arrowRight from '../../assets/arrowRight.png';
 import {useState} from 'react';
 import {useParams} from 'react-router-dom';
 
 function Fiches(){
     const {idLogements} = useParams();
-    const test = data.indexOf({idLogements});
-    const [index, setIndex] = useState(0);
-    const dat = data.filter(e => e === data[index]);
+    const logement = (e) => e.id === idLogements;
+    const test = data.findIndex(logement);
+    const [pics, setPics] = useState(0)
+    const dat = data.filter(e => e === data[test]);
 
     function nextPics(){
-        setIndex(index + 1)
+        setPics(pics + 1)
         }
+    function prevPics(){
+        setPics(pics -1)
+    }
         
     return(
         <section>
             {dat.map((loc) => (
-                <div key={loc.id} className="fiche" >
-                    <Slideshow  img={[loc.pictures[index]]} onClick={nextPics}/>
-                    <Title title={loc.title} location={loc.location} />
-                    <div className="loc_info-tags">
-                        {loc.tags.map((tags) => (
-                            <Tags key={`${loc.id}-${tags}`} text={tags}/>
-                        ))}
+                <div key={loc.id} className="fiche"  >
+                    <div className="arrow">
+                        <img className="arrow_right" src={arrowRight} alt="" onClick={nextPics} />
+                        <img className="arrow_left" src={arrowLeft} alt="" onClick={prevPics} />
                     </div>
-                    <div className="loc_info-collapse">
-                        <Collapse title="Description" content={loc.description}/>
-                        <Collapse title="Équipements" content={loc.equipments} />
+                    <Slideshow  img={[loc.pictures[pics]]} />
+                    <div className="loc_info">
+                        <Title title={loc.title} location={loc.location} />
+                        <div className="loc_info-tags">
+                            {loc.tags.map((tags) => (
+                            <Tags key={`${loc.id}-${tags}`} text={tags}/>
+                            ))}
+                        </div>
+                        <div className="loc_info-collapse">
+                            <Collapse title="Description" content={loc.description}/>
+                            <Collapse title="Équipements" content={loc.equipments} className='collapse_equipements' />
+                        </div>
                     </div>
                 </div>
             ))}
