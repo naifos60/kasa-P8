1dd349f1617f189163a51d00ae573775d76eab72
naifos60 <naifos60@hotmail.com>
2023-06-18 20:26:26 +0200

    feature boucle carrousel

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -7,11 +7,18 @@ function Slideshow({img}){
     const test = (nextPics) => {setPics(nextPics(pics))};
     const len = picture.length;
 
-    
+    function resetSlide(){
+        if(pics === len){
+            pics = 0;
+        }else if(pics < 0){
+            pics = len - 1;
+        }
+        return pics
+    }
     
     return(   
     <div className="slideshow">
-        <img src={picture[pics]} className="slideshow_image"  alt='vue loc'/>
+        <img src={picture[resetSlide()]} className="slideshow_image"  alt='vue loc'/>
         {`${len}` > 1 &&
             <div>
                 <Arrow test={test}/>
