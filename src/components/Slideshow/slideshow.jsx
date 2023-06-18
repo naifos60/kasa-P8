f74807b56474796f40a5e606e8890c4062aed7c1
naifos60 <naifos60@hotmail.com>
2023-06-18 19:49:50 +0200

    feature non affichage fleche et dot si une seul img

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -5,13 +5,18 @@ function Slideshow({img}){
     let [pics, setPics] = useState(0);
     const picture = img;
     const test = (nextPics) => {setPics(nextPics(pics))};
+    const len = picture.length;
+
+    
     
     return(   
     <div className="slideshow">
-        
         <img src={picture[pics]} className="slideshow_image"  alt='vue loc'/>
-        <Arrow test={test}/>
-        <div className="dot"><p>{(pics + 1)} / {(picture.length)}</p></div>
+        {`${len}` > 1 &&
+            <div>
+                <Arrow test={test}/>
+                <div className="dot"><p>{(pics + 1)} / {(picture.length)}</p></div> 
+            </div>}
     </div>
     )
 }
