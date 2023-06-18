be82779f1c7fb374a0ecfd55f5527689e9b02a1b
naifos60 <naifos60@hotmail.com>
2023-06-18 12:49:44 +0200

    refactor slideshow

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -2,7 +2,7 @@ import Arrow from "../Arrow/arrow.jsx";
 import {useState} from 'react'
 
 function Slideshow({img}){
-    const [pics, setPics] = useState(0);
+    let [pics, setPics] = useState(0);
     const picture = img;
     const test = (nextPics) => {setPics(nextPics(pics))};
     return(
