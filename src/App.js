a65bdd7e9fd732736271dc7f60be87be57c0ca01
naifos60 <naifos60@hotmail.com>
2023-06-20 20:40:14 +0200

    commit final

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/home.jsx';
+import Propos from './pages/APropos/aPropos.jsx';
+import Header from './components/Header/header.jsx';
+import Footer from './components/Footer/footer.jsx';
+import Error from './pages/Error/error.jsx';
+import Fiches from './pages/Fiches/fiches.jsx';
+
+function App(){
+    return(
+    <Router>
+      <Header />
+        <Routes> 
+          <Route path='/' element={<Home />} />
+          <Route path='/propos' element={<Propos />} />
+          <Route path='*' element={<Error />} />
+          <Route path='/fiches/:idLogements' element={<Fiches />} />
+        </Routes>
+      <Footer />
+    </Router>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './utils/styles/css/index.css';
-import Home from './pages/Home/home.jsx';
-import Propos from './pages/APropos/aPropos.jsx';
-import Header from './components/Header/header.jsx';
-import Footer from './components/Footer/footer.jsx';
-import Error from './pages/Error/error.jsx';
-import Fiches from './pages/Fiches/fiches.jsx';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-        <Routes> 
-          <Route path='/' element={<Home />} />
-          <Route path='/propos' element={<Propos />} />
-          <Route path='*' element={<Error />} />
-          <Route path='/fiches/:idLogements' element={<Fiches />} />
-        </Routes>
-      <Footer />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
